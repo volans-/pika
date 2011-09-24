3ff3a54c0ee12076b7d1f8d8b2c5a0167c755788
Gavin M. Roy <gmr@myyearbook.com>
2011-09-24 18:12:17 -0400

    Add a test for Connection.Open and Connection.Tune, add the channel to the the ProtocolHeader demarshal test.

--- tests/frame_tests.py
+++ tests/frame_tests.py
@@ -52,7 +52,7 @@ def test_protocol_header_demarshal():
     frame_data = u'AMQP\x00\x00\t\x01'
 
     # Decode the frame and validate lengths
-    bytes_read, test = amqp.frame.demarshal(frame_data)
+    bytes_read, channel, test = amqp.frame.demarshal(frame_data)
 
     if test.__class__.__name__ != 'ProtocolHeader':
         assert False, "Invalid Frame Type: %s" % test.__class__.__name__
@@ -66,3 +66,66 @@ def test_protocol_header_demarshal():
         test.revision) != amqp.definitions.AMQP_VERSION:
         assert False, "Invalid Protocol Version: %i-%i-%i" % \
                       (test.major_version, test.minor_version, test.revision)
+
+def test_connection_start_demarshal():
+    frame_data = (u'\x01\x00\x00\x00\x00\x01G\x00\n\x00\n\x00\t\x00\x00\x01'
+                  u'"\x0ccapabilitiesF\x00\x00\x00X\x12publisher_confirmst\x01'
+                  u'\x1aexchange_exchange_bindingst\x01\nbasic.nackt\x01\x16'
+                  u'consumer_cancel_notifyt\x01\tcopyrightS\x00\x00\x00'
+                  u'$Copyright (C) 2007-2011 VMware, Inc.\x0binformationS\x00'
+                  u'\x00\x005Licensed under the MPL.  See http://www.rabbitmq'
+                  u'.com/\x08platformS\x00\x00\x00\nErlang/OTP\x07productS\x00'
+                  u'\x00\x00\x08RabbitMQ\x07versionS\x00\x00\x00\x052.6.1\x00'
+                  u'\x00\x00\x0ePLAIN AMQPLAIN\x00\x00\x00\x05en_US\xce')
+
+    expectation = {'version_major': 0,
+                   'version_minor': 9,
+                   'server_properties':
+                       {u'information': (u'Licensed under the MPL.  '
+                                          'See http://www.rabbitmq.com/'),
+                        u'product': u'RabbitMQ',
+                        u'copyright': u'Copyright (C) 2007-2011 VMware, Inc.',
+                        u'capabilities': {u'exchange_exchange_bindings': True,
+                                          u'consumer_cancel_notify': True,
+                                          u'publisher_confirms': True,
+                                          u'basic.nack': True},
+                      u'platform': u'Erlang/OTP',
+                      u'version': u'2.6.1'},
+                   'mechanisms': u'PLAIN AMQPLAIN',
+                   'locales': u'en_US'}
+
+    # Decode the frame and validate lengths
+    consumed, channel, frame = amqp.frame.demarshal(frame_data)
+
+    # Validate the bytes consumed
+    if consumed != 335:
+        assert False, 'Bytes consumed did not match the expected value'
+
+    # Validate the channel
+    if channel != 0:
+        assert False, 'Channel number did not match the expected value'
+
+    # Run the frame check, assertions contained within
+    test_support.check_frame(frame, expectation)
+
+def test_connection_tune_demarshal():
+    frame_data = (u'\x01\x00\x00\x00\x00\x00\x0c\x00\n\x00\x1e\x00\x00\x00\x02'
+                  u'\x00\x00\x00\x00\xce')
+
+    expectation = {'channel_max': 0,
+                   'frame_max': 131072,
+                   'heartbeat': 0}
+
+    # Decode the frame and validate lengths
+    consumed, channel, frame = amqp.frame.demarshal(frame_data)
+
+    # Validate the bytes consumed
+    if consumed != 20:
+        assert False, 'Bytes consumed did not match the expected value'
+
+    # Validate the channel
+    if channel != 0:
+        assert False, 'Channel number did not match the expected value'
+
+    # Run the frame check, assertions contained within
+    test_support.check_frame(frame, expectation)
