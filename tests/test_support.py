fc374fd28fc4859508690e181eec5bb92ab9c094
Gavin M. Roy <gmr@myyearbook.com>
2011-09-24 01:17:35 -0400

    Move functions from the codec tests into this test support module.

--- tests/test_support.py
+++ tests/test_support.py
@@ -8,5 +8,104 @@ __author__ = 'Gavin M. Roy'
 __email__ = 'gmr@myyearbook.com'
 __date__ = '2011-03-29'
 
+from decimal import Decimal
 import sys
+from time import mktime, struct_time
+
 sys.path.insert(0, '..')
+
+
+def validate_attribute(method, attribute, attribute_type, value='ignore'):
+    """
+    Validate that the given method object has the specified attribute of the
+    specified attribute_type. If a value is passed in, validate that as well
+    """
+    if not hasattr(method, attribute):
+        assert False, "%s missing %s attribute" % (method, attribute)
+
+    if getattr(method, attribute) and \
+       not isinstance(getattr(method, attribute), attribute_type):
+        assert False, "%s.%s is not %s" % \
+                      (method, attribute, attribute_type)
+
+    if value != 'ignore' and value != getattr(method, attribute):
+        assert False, "Expected a value of %r, received %r" % \
+                      (value, getattr(method, attribute))
+
+
+def compare_lists(source, dest, method='Unknown'):
+    for position in xrange(0, len(source)):
+
+        # Validate decimal, int, long, str, nothing special needed
+        if isinstance(source[position], Decimal) or \
+           isinstance(source[position], int) or \
+           isinstance(source[position], long) or \
+           isinstance(source[position], str):
+            if source[position] != dest[position]:
+                assert False, \
+                "%s did not properly decode item in position %i '%r' vs '%r'" \
+                % (method, position, source[position], dest[position])
+
+        elif isinstance(source[position], float):
+            if round(source[position], 2) != round(dest[position], 2):
+                assert False, \
+                "%s did not properly decode item in position %i '%r' vs '%r'" \
+                % (method, position, source[position], dest[position])
+
+        elif isinstance(source[position], struct_time):
+            if mktime(source[position]) != mktime(dest[position]):
+                assert False, \
+                "%s did not properly decode item in position %i '%r' vs '%r'" \
+                % (method, position, source[position], dest[position])
+
+        elif isinstance(source[position], list):
+            compare_lists(source[position], dest[position], method)
+
+        elif isinstance(source[position], dict):
+            compare_dicts(source[position], dest[position], method)
+
+        else:
+            assert False, "Unexpectationed item in position %i: %r" % \
+                          (position, source[position])
+
+
+def compare_dicts(source, dest, method='Unknown'):
+    for key in source.keys():
+
+        # Validate we have the key
+        if key not in dest:
+            assert False, "%s did not properly decode dict: %s missing: %r" % \
+                          (method, key, dest)
+
+        # Validate decimal, int, long, str, nothing special needed
+        if isinstance(source[key], Decimal) or \
+           isinstance(source[key], int) or \
+           isinstance(source[key], long) or \
+           isinstance(source[key], str):
+            if source[key] != dest[key]:
+                assert False, \
+                "%s did not properly decode item %s '%r' vs '%r'" \
+                % (method, key, source[key], dest[key])
+
+        elif isinstance(source[key], float):
+            if round(source[key], 2) != round(dest[key], 2):
+                assert False, \
+                "%s did not properly decode item %s '%r' vs '%r'" \
+                % (method, key, source[key], dest[key])
+
+        elif isinstance(source[key], struct_time):
+            if mktime(source[key]) != mktime(dest[key]):
+                assert False, \
+                "%s did not properly decode item %s '%r' vs '%r'" \
+                % (method, key, source[key], dest[key])
+
+        elif isinstance(source[key], list):
+            compare_lists(source[key], dest[key], method)
+
+        elif isinstance(source[key], dict):
+            compare_dicts(source[key], dest[key], method)
+
+        else:
+            assert False, "Unexpectationed item %s: %r" % \
+                          (key, source[key])
+
