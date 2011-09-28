4c9e9a301e57fced3e789c513dca6b18a8f82eb9
Gavin M. Roy <gmr@myyearbook.com>
2011-09-27 22:35:49 -0400

    Rename definitions to specification

--- pika/amqp/__init__.py
+++ pika/amqp/__init__.py
@@ -6,8 +6,8 @@ AMQP Specifications and Classes
 __author__ = 'Gavin M. Roy <gmr@myyearbook.com>'
 __since__ = '2011-09-23'
 
-import definitions
 import frame
 import header
+import specification
 
 from header import ProtocolHeader
