758be7fb164350437579637849556715676e2bad
Gavin M. Roy <gmr@myyearbook.com>
2012-03-25 19:20:11 -0400

    Fix PlainAuth
    - Use the specification constants for default username and password
    - Update the docstrings
    - Fix the bug in marshall

--- pika/authentication/plain.py
+++ pika/authentication/plain.py
@@ -6,21 +6,32 @@ __author__ = 'Gavin M. Roy'
 __email__ = 'gmr@myyearbook.com'
 __date__ = '2011-10-16'
 
-from . import auth
+from pika.authentication import auth
+from pika.amqp import specification
 
 
 class PlainAuth(auth.Auth):
+    """Class for specifying the credentials to connect with when using AMQP
+    PlainAuth.
 
-    def __init__(self, username, password):
-        """Create the PlainAuth object that will
+    """
+    def __init__(self, username=None, password=None):
+        """Create the PlainAuth object for authentication that defaults to the
+        specification default user and default pass.
+
+        :param str username: The username to connect as, if omitted uses
+                             pika.amqp.specification.DEFAULT_USER
+        :param str password: The password to connect with, if omitted, use
+                             pika.amqp.specification.DEFAULT_PASS
 
         """
-        self._username = username
-        self._password = password
+        self._username = username or specification.DEFAULT_USER
+        self._password = password or specification.DEFAULT_PASS
 
     def marshal(self):
         """Return the marshaled auth credentials
 
         :returns str: The marshaled string
+
         """
-        return '\0%s\0%s' % self._username, self._password
+        return '\0%s\0%s' % (self._username, self._password)
