3cee463302c0c53f25547dec45bbdbc0ac7a75c3
Gavin M. Roy <gavinmroy@gmail.com>
2014-02-03 12:00:34 -0500

    Add URL parsing functionality that is Python 2 & 3 Compatible
    - Used for AMQP URL parsing

--- pika/utils.py
+++ pika/utils.py
@@ -3,6 +3,10 @@ Non-module specific functions shared by modules in the pika package
 
 """
 import collections
+try:
+    from urllib import parse as _urlparse
+except ImportError:
+    import urlparse as _urlparse
 
 from pamqp import PYTHON3
 
@@ -28,3 +32,25 @@ def is_string(value):
     if not PYTHON3:
         checks.append(isinstance(value, unicode))
     return any(checks)
+
+
+Parsed = collections.namedtuple('Parsed',
+                                'scheme,netloc,path,params,query,fragment,'
+                                'username,password,hostname,port')
+
+
+def parse_qs(query_string):
+    return _urlparse.parse_qs(query_string)
+
+
+def urlparse(url):
+    value = 'http%s' % url[4:] if url[:4] == 'amqp' else url
+    parsed = _urlparse.urlparse(value)
+    return Parsed(parsed.scheme.replace('http', 'amqp'), parsed.netloc,
+                  parsed.path, parsed.params, parsed.query, parsed.fragment,
+                  parsed.username, parsed.password, parsed.hostname,
+                  parsed.port)
+
+
+def unquote(value):
+    return _urlparse.unquote(value)
