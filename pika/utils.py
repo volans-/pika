3618f2551f83ffc4e1d6447f6aaa2084febf2f1b
Gavin M. Roy <gavinmroy@gmail.com>
2014-02-03 10:47:49 -0500

    Add is_string check for Python 3 support
    Use instead of checking for basestring, not supported in Python 3.2

--- pika/utils.py
+++ pika/utils.py
@@ -4,6 +4,8 @@ Non-module specific functions shared by modules in the pika package
 """
 import collections
 
+from pamqp import PYTHON3
+
 
 def is_callable(handle):
     """Returns a bool value if the handle passed in is a callable
@@ -14,3 +16,15 @@ def is_callable(handle):
 
     """
     return isinstance(handle, collections.Callable)
+
+
+def is_string(value):
+    """Evaluate a value and check to see if it's a string.
+
+    :rtype: bool
+
+    """
+    checks = [isinstance(value, bytes), isinstance(value, str)]
+    if not PYTHON3:
+        checks.append(isinstance(value, unicode))
+    return any(checks)
--- tests/utils_tests.py
+++ tests/utils_tests.py
@@ -1,3 +1,4 @@
+import sys
 try:
     import unittest2 as unittest
 except ImportError:
@@ -13,3 +14,22 @@ class UtilsTests(unittest.TestCase):
 
     def test_is_callable_false(self):
         self.assertFalse(utils.is_callable(1))
+
+    def test_is_string_str(self):
+        self.assertTrue(utils.is_string('foo'))
+
+    def test_is_string_bytes(self):
+        self.assertTrue(utils.is_string(b'foo'))
+
+    @unittest.skipIf(sys.version_info[0] == 3, 'No unicode obj in 3')
+    def test_is_string_unicode(self):
+        self.assertTrue(utils.is_string(unicode('foo')))
+
+    def test_is_string_false_int(self):
+        self.assertFalse(utils.is_string(123))
+
+    def test_is_string_false_dict(self):
+        self.assertFalse(utils.is_string({'foo': 'bar'}))
+
+    def test_is_string_false_list(self):
+        self.assertFalse(utils.is_string(['foo', 'bar']))
